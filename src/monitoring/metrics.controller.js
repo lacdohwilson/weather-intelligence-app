9aaf88de5a048d3b1eab8bb39b6b98fd2c2e4e1a
lacdohwilson <lacdohwilsontchofor@gmail.com>
2026-06-10 20:11:40 +0100

    [feat] - render metrics data

---
+++ src/monitoring/metrics.controller.js
@@ -0,0 +1,12 @@
+import { register } from './metrics.js';
+import catchAsync from '../utils/catchAsync.js';
+
+const getMetrics = catchAsync(async (req, res, next) => {
+    const metrics = await register.metrics();
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
+
+export default {
+    getMetrics,
+};

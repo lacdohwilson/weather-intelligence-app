b0674af7e3836423f7458066d368a965b7e5b242
lacdohwilson <lacdohwilsontchofor@gmail.com>
2026-06-10 20:12:26 +0100

    [ref] - Order imports

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express, { json, urlencoded } from "express";
 import swaggerUi from 'swagger-ui-express';
 import weatherRouter from "./routes/weather.routes.js";
+import metricsRouter from "./monitoring/metrics.routes.js";
+import metricsMiddleware from "./monitoring/metrics.middleware.js";
 import globalErrorHandler from "./controllers/error.controller.js";
 import config from "./configurations/config.js";
 import AppError from "./utils/appError.js";
@@ -30,8 +32,12 @@ app.use(requestLogger);
 // SWAGGER DOCUMENTATION
 app.use(`${config.prefix}/api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// METRICS
+app.use(metricsMiddleware);
+
 // ROUTES
 app.use(`${config.prefix}/weather`, weatherRouter);
+app.use(`${config.prefix}/metrics`, metricsRouter);
 
 //INVALID ROUTES
 app.use((req, res, next) => {
